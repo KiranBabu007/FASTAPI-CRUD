f7fcc38bfeb829a6465ca0741ca85a182ba12511
Kiran <kiranbabu3355@gmail.com>
2025-06-30 12:26:44 +0530

    Add HTTPException handling to update and delete book endpoints

--- books.py
+++ books.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException
 from pydantic import BaseModel,Field
 from uuid import UUID
 
@@ -23,4 +23,26 @@ def read_api():
 def create_book(book:Book):
     BOOKS.append(book)
     return book
+
+@app.put("/{book_id}")
+def update_book(book_id:UUID,book:Book):
+    counter = 0
+
+    for x in BOOKS:
+        counter+=1
+        if x.id ==book_id:
+            BOOKS[counter-1]=book
+            return BOOKS[counter-1]
+    raise HTTPException(status_code=404,detail=f"ID {book_id} not found")
+
+@app.delete("/{book_id}")
+def delete_book(book_id:UUID):
+    counter = 0
+
+    for x in BOOKS:
+        counter+=1
+        if x.id == book_id:
+            BOOKS.pop(counter-1)
+            return(f"Book with bookid {book_id} deleted successfully")
+    raise HTTPException(status_code=404,detail=f"ID {book_id} not found")
 
