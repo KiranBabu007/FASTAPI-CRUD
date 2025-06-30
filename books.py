84c7118d4af75f34693f34aa7487c38e6566aed2
Kiran <kiranbabu3355@gmail.com>
2025-06-30 12:12:00 +0530

    Implement Book model and update API endpoints for book management

--- books.py
+++ books.py
@@ -1,7 +1,25 @@
 from fastapi import FastAPI
+from pydantic import BaseModel,Field
+from uuid import UUID
 
 app = FastAPI()
 
-@app.get("/{name}")
+class Book(BaseModel):
+    id:UUID
+    title:str = Field(min_length=1)
+    author:str = Field(min_length=1,max_length=100)
+    description: str = Field(min_length=1, max_length=500)
+    rating:int = Field(gt=-1,lt=101)
+
+BOOKS = []
+
+
+
+@app.get("/")
 def read_api(name:str):
-    return {'Welcome':'Kiran'}
+    return BOOKS
+
+@app.post("/")
+def create_book(book:Book):
+    BOOKS.append(book)
+    return book
